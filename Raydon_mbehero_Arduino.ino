5674f02a786e390954fac99d8f298883bd022d77
muregii <88530358+muregii@users.noreply.github.com>
2022-10-01 21:13:34 -0400

    Add files via upload
    This file toggles between Two LED States. On And OFF

---
+++ Raydon_mbehero_Arduino.ino
@@ -0,0 +1,56 @@
+// C++ code
+int LEDState=0;
+int LEDState2=0;
+int LEDPin=13;
+int buttonPin=2;
+int LEDPin2=12;
+int buttonPin2=4;
+int buttonNew;
+int buttonNew2;
+int buttonOld=1;
+int buttonOld2=1;
+int dt=100;
+
+void setup() {
+  // setting up inputs and output pins:
+  Serial.begin(9600);
+  pinMode(LEDPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(LEDPin2, OUTPUT);
+  pinMode(buttonPin2, INPUT);
+  
+}
+
+void loop() {
+  // Reading between the state sof the button to determine LED State:
+  buttonNew=digitalRead(buttonPin);
+  buttonNew2=digitalRead(buttonPin2);
+  if(buttonOld == 0 && buttonNew == 1) {
+    if(LEDState==0){
+      digitalWrite(LEDPin, HIGH);
+      LEDState=1;
+    }
+    else{
+      digitalWrite(LEDPin, LOW);
+      LEDState=0;
+    }
+  }
+  
+buttonOld=buttonNew;
+delay(dt);
+  
+  if(buttonOld2 == 0 && buttonNew2 == 1) {
+    if(LEDState2==0){
+      digitalWrite(LEDPin2, HIGH);
+      LEDState2=1;
+    }
+    else{
+      digitalWrite(LEDPin2, LOW);
+      LEDState2=0;
+    }
+  }
+  
+  
+buttonOld2=buttonNew2;
+delay(dt);
+}
